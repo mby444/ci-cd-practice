03ab0ff5b9dc5a42229f2ad9b13a6cf128a4fe49
mby444 <bimayudha161@gmail.com>
2025-02-24 17:52:15 +0700

    Fix Hello World message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "hello world!" });
+  res.json({ message: "Hello World!" });
 });
 
 app.get("/:name", (req, res) => {
